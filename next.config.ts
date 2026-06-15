0d94d317c0cda1a218cea38823b901ae3f217016
PichoX <itzcarlosandres@gmail.com>
2026-06-15 01:01:37 -0500

    fix: disable next/image optimization to support base64 data URIs

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '*', pathname: '/**' },
-      { protocol: 'http', hostname: '*', pathname: '/**' },
-    ],
+    unoptimized: true,
   },
 };
 
